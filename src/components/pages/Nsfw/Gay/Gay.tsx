a3274f76e894917f56e7e9cfe9333d345d196f13
Your Name <you@example.com>
2023-09-29 22:25:32 -0300

    first commit

--- src/components/pages/Nsfw/Gay/Gay.tsx
+++ src/components/pages/Nsfw/Gay/Gay.tsx
@@ -1,6 +1,7 @@
 import Footer from "../../../Footer/Footer"
 import Header from "../../../Header/Header"
 import img from "../../../../Arquivos/Feline/001.jpg"
+import img1 from "../../../../Arquivos/Amumu/1.jpg"
 // css
 import style from "./Gay.module.css"
 import { Link } from "react-router-dom"
@@ -12,6 +13,13 @@ const Gay = () => {
     
         <section className={style.pig}>
           <div className={style.cont}>
+          <div className={style.ft}>
+              <h1>Besta x Selva</h1>
+        <Link to={"/Amumu"}>
+        <img src={img1} alt="" />
+        </Link>
+        <h2>Tradutor: Jeredy</h2>
+            </div>
             <div className={style.ft}>
               <h1>Feline Cafe</h1>
         <Link to={"/felinecafe"}>
